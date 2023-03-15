935c1b623279a1a17e0b65526f886e216c2b1997
amolaher92 <amolaher92@gmail.com>
2023-03-15 21:04:51 +0530

    updated vite config and tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,217 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+          <div>
+            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              Brand new
+            </p>
+          </div>
+          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span className="relative inline-block">
+              <svg
+                viewBox="0 0 52 24"
+                fill="currentColor"
+                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+              >
+                <defs>
+                  <pattern
+                    id="18302e52-9e2a-4c8e-9550-0cbb21b38e55"
+                    x="0"
+                    y="0"
+                    width=".135"
+                    height=".30"
+                  >
+                    <circle cx="1" cy="1" r=".7" />
+                  </pattern>
+                </defs>
+                <rect
+                  fill="url(#18302e52-9e2a-4c8e-9550-0cbb21b38e55)"
+                  width="52"
+                  height="24"
+                />
+              </svg>
+              <span className="relative">The</span>
+            </span>{" "}
+            quick, brown fox jumps over a lazy dog
+          </h2>
+          <p className="text-base text-gray-700 md:text-lg">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque rem aperiam, eaque ipsa quae.
+          </p>
+        </div>
+        <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+            <div>
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <h6 className="mb-2 font-semibold leading-5">The deep ocean</h6>
+              <p className="mb-3 text-sm text-gray-900">
+                A flower in my garden, a mystery in my panties. Heart attack
+                never stopped old Big Bear.
+              </p>
+            </div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+          <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+            <div>
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <h6 className="mb-2 font-semibold leading-5">When has justice</h6>
+              <p className="mb-3 text-sm text-gray-900">
+                Rough pomfret lemon shark plownose chimaera southern sandfish
+                kokanee northern sea.
+              </p>
+            </div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+          <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+            <div>
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <h6 className="mb-2 font-semibold leading-5">Organically grow</h6>
+              <p className="mb-3 text-sm text-gray-900">
+                A slice of heaven. O for awesome, this chocka full cuzzie is as
+                rip-off as a cracker.
+              </p>
+            </div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+          <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+            <div>
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <h6 className="mb-2 font-semibold leading-5">
+                A slice of heaven
+              </h6>
+              <p className="mb-3 text-sm text-gray-900">
+                Disrupt inspire and think tank, social entrepreneur but
+                preliminary thinking think tank compelling.
+              </p>
+            </div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      <button
+        type="button"
+        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Purple to Blue
+      </button>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Cyan to Blue
+      </button>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Green to Blue
+      </button>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Purple to Pink
+      </button>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Pink to Orange
+      </button>
+      <button
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Teal to Lime
+      </button>
+      <button
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Red to Yellow
+      </button>
+    </>
+  );
 }
-
-export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/assets"),
+      "~": path.resolve(__dirname, "src/components"),
+    },
+  },
+  build: {
+    sourcemap: true,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(extType)) {
+            extType = "img";
+          } else if (/woff|woff2|ttf|eot/.test(extType)) {
+            extType = "fonts";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+  },
+});
